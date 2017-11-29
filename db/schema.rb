fa7d85f5e1ae9ca4e0f0ca555f974671748f9479
Shaila Kavrakova <shaila.kavrakova@gmail.com>
2017-11-28 20:58:16 -0500

    Update the brick picture

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 20171128014211) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "review"
-    t.integer "picture_id"
+    t.bigint "picture_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["picture_id"], name: "index_comments_on_picture_id"
@@ -36,4 +39,5 @@ ActiveRecord::Schema.define(version: 20171128014211) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "pictures"
 end
